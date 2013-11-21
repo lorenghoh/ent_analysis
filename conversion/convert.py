dff791985bad659d45420ffe3d8d9fde82d8ed31
Loren Oh <loren.gh.oh@gmail.com>
2013-11-21 17:07:13 -0500

    Convert.py now also translates .stat file

--- conversion/convert.py
+++ conversion/convert.py
@@ -1,24 +1,37 @@
 #!/usr/bin/python                                                                                             
-import os, glob
+import os, glob, shutil
 import multiprocessing as mp
-from multiprocessing import Pool
 
 import model_param as mc
 
 PROC = 16
 
 FILELIST = glob.glob(mc.input_directory + '*.com3D')
-CONVERTER = mc.sam_directory + '/UTIL/com3D2nc'
+SAM = mc.sam_directory
+CONVERTER = SAM + '/UTIL/com3D2nc '
 
 ############################
 ### No need to edit the following script
-def convert(filename):
-	result = os.system(CONVERTER + ' ' + filename)
+def convert(filename):	
+	result = os.system(CONVERTER + filename)
 	print result
 	
 	if result != 0:
 		print "Process aborted."
 		raise "Conversion failed!"
+		
+def convert_stat():
+	stat_name = glob.iglob(SAM + '/OUT_STAT/*.stat').next()
+	nc_name = stat_name.split('.')[0] + '.nc'
+	
+	result = os.system(SAM + '/UTIL/stat2nc ' + stat_name)
+	print result
+	
+	if result != 0:
+		print "Process aborted."
+		raise "Conversion failed!"
+	else:
+		shutil.move(nc_name, mc.data_directory)
 		
 def main():
 	pool = mp.Pool(PROC)
